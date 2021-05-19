4a04a11f1cd7a0b0460ee794899bc354a26a3349
Taco <verobere28@gmail.com>
2021-05-19 12:41:45 -0500

    mongoose API koders

--- backend/mongooseAPI/index.js
+++ backend/mongooseAPI/index.js
@@ -1,6 +1,11 @@
 
+const { response } = require('express')
 const express = require('express')
 const mongoose = require('mongoose')
+const Koder = require('./koderModel')
+const server = express()
+
+server.use(express.json())
 
 const DB_USER = 'TacoUser'
 const DB_PASSWORD = 'taco123'
@@ -9,26 +14,83 @@ const DB_NAME = 'kodemia'
 
 const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
-const server = express()
 
-server.get('/koders', (requeste,response) => {
+server.get('/koders', async (request, response) => {
+
+  const allKoders = await Koder.find({})
+
   response.json({
     message: 'all koders',
     success: true,
     data: {
-      koders: []
+      koders: allKoders
     }
   })
 })
 
+server.get('/koders/:genderKoder', async (request, response) => {
+  const genderK = request.params.genderKoder
+  let allKoders = await Koder.find({})
+  let filteredKoderByGender = genderK ? allKoders.filter(koder => koder.gender == genderK) : null
 
+  if (!filteredKoderByGender) {
+    response.status(404)
+    response.json({
+      success: false,
+      message: 'No hay koders disponibles :c'
+    })
+    return
+  }
+  if (filteredKoderByGender) {
+    allKoders = filteredKoderByGender
+    response.json({
+      success: true,
+      message: 'Se encontraron coincidencias',
+      data: {
+        koders: allKoders
+      }
+    })
+  }
+})
 
-mongoose.connect(url, { userNewUrlParser: true,  useUnifiedTopology: true})
-.then((conn) => {
-  server.listen(8080, () => {
-    console.log('server is listening')
+server.post('/koders', async (request, response) => {
+  const name = request.body.name
+  const lastName = request.body.lastName
+  const age = request.body.age
+  const gender = request.body.gender
+
+  const newKoder = { name, lastName, age, gender }
+
+  console.log(newKoder)
+
+  await Koder.create(newKoder)
+
+  response.json({
+    success: true,
+    message: 'Se ha agregado un nuevo Koder'
   })
 })
-.catch(error => {
-  console.error('error: ',error)
-})
+
+mongoose.connect(url, { userNewUrlParser: true, useUnifiedTopology: true })
+  .then((conn) => {
+    server.listen(8080, () => {
+      console.log('server is listening')
+    })
+  })
+  .catch(error => {
+    console.error('error: ', error)
+  })
+
+
+/*
+practica
+
+GET /koders
+-gender
+
+-POST /koders
+-name
+-lastname-
+-age
+-gender
+*/
